6fe4347ba1bc00943f3f043d41e6909970618084
soyjoysauce <soyjoysauce@gmail.com>
2017-11-25 20:18:35 -0800

    experimenting moving away from jquery to use vanilla javascript to manipulate the DOM elements

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-$(document).ready(init);
+// $(document).ready(init);
+// function init(){
+//     board = new Board();
+//     controller = new Controller();
+//     model = new Model();
+//     console.log('init and things are made');
+// }
+//moving away from jquery
+document.addEventListener('onload',init());
 
-function init(){
-    board = new Board();
-    constroller = new Controller();
-    model = new Model();
-    console.log('init and things are made');
-}
-
+function init() {
 // Board randomly generates pieces. 
 // Board sends the most current state of the board to Controller 
-// Board recieves the updated state of the board from the Model 
+// Board receives the updated state of the board from the Model 
 class Board {
     constructor(){
         this.jewel_arr = [];
@@ -24,38 +26,47 @@ class Board {
         let counter = 1;
         let piecesArr = this.pieces_arr;
         let jewelArr = this.jewel_arr;
-        
-        
-    while(jewelArr.length < 64 ){
-        piecesArr.map(function(){ 
-            let randomNum = Math.floor(Math.random() * piecesArr.length);                                        
-            let tile = piecesArr[randomNum];                            
-            let axisArr = [];
 
-            let jewelPiece = {
-                'x': x_axis,
-                'y' : y_axis,
-                'id' : counter , 
-                'tile' : tile,
-                'info' : $('<div>', {
-                    'tile' : tile,
-                    'x' : x_axis,
+        // let containerDiv = document.documentElement.getElementsById('gameTile');
+        let containerDiv = document.documentElement.getElementsByClassName('grid-div')
+        console.log('containerDiv:', containerDiv);
+        let game_grid = document.documentElement.querySelectorAll('.game_grid_container');
+        console.log('game_grid:',game_grid);
+        while(jewelArr.length < 64 ){
+            piecesArr.map(function(){ 
+                let randomNum = Math.floor(Math.random() * piecesArr.length);                                        
+                let tile = piecesArr[randomNum];                            
+                let axisArr = [];                
+                let jewelPiece = {
+                    'x': x_axis,
                     'y' : y_axis,
-                    'id' : counter
-                })
-            };
-
-            console.log('tile', tile);
-            console.log('jewelPiece', jewelPiece);
-            $('.game_grid_container').append(jewelPiece.info.clone());
-            axisArr.push(jewelPiece);
-            jewelArr.push(axisArr); 
-            x_axis++;  
-            y_axis++;
-            counter++;                               
-            })
-            return jewelArr;
-    }   
+                    'id' : counter , 
+                    'tile' : piecesArr[randomNum],
+                    'class': piecesArr[randomNum],
+                    'info' : {
+                        'tile' : piecesArr[randomNum],
+                        'x' : x_axis,
+                        'y' : y_axis,
+                        'id' : counter
+                    }
+                };
+                console.log('jewelPiece:', jewelPiece);
+                let newDiv  = document.createElement('div',tile);
+                // let jewelClone = Object.assign(newDiv, jewelPiece);
+                console.log('newDiv:',newDiv);
+                // let newDiv = document.createElement(jewelClone);
+                // console.log('newDiv:',newDiv);
+                document.body.insertBefore(newDiv,containerDiv);
+                // $('.game_grid_container').append(jewelPiece.info.clone());
+                axisArr.push(jewelPiece);
+                jewelArr.push(axisArr); 
+                x_axis++;  
+                y_axis++;
+                counter++;                               
+            }) 
+        }
+        let board_state = [...jewelArr];
+        this.sendControllerBoardState(board_state);        
 
     }
 
@@ -63,8 +74,11 @@ class Board {
         
     }
 
-    sendControllerBoardState(){
-
+    sendControllerBoardState(board_state){
+        //if there is something in the jewelArr -- send this to controller state
+        let boardState = [...board_state];
+        boardState !== null ? Controller.boardState = board_state : 'error';
+        console.log('Controller.boardState:',Controller.boardState);
     }
 
 
@@ -77,24 +91,22 @@ class Controller {
         this.clickHandlers();
         this.first_click = null;
         this.second_click = null;
+        this.boardState = [];
+        
     }
     
     clickHandlers(){
-        $('.game_grid_container').on('click','div',function(){
-            console.log('click')
-            game_board.clicked($(this).attr('x'), $(this).attr('y'), $(this).attr('id'));
-        });
-        $('.game_grid_container').on('click','.clickable',function(){
-            console.log('click')
-            game_board.clicked($(this).attr('x'), $(this).attr('y'), $(this).attr('id'));
-        });
-        $('.game_reset_button').on('click', function(){
-            location.reload();
-            model.displayStats();
-        });
+        // $('.game_grid_container').on('click','div',function(){
+        //     console.log('click')
+        //     board.clicked($(this).attr('x'), $(this).attr('y'), $(this).attr('id'));
+        // });
+        // $('.game_reset_button').on('click', function(){
+        //     location.reload();
+        //     model.displayStats();
+        // });
     }
 
-    recieveBoardState(){
+    receiveBoardState(){
 
     }
 
@@ -123,7 +135,7 @@ class Model {
 
     }
 
-    recieveControllerState(){
+    receiveControllerState(){
 
     }
 
@@ -137,6 +149,10 @@ class Model {
 
 }
 
-let board = null;
-let controller = null;
-let model = null; 
+let board = this.board = new Board;
+let controller = this.controller = new Controller;
+let model = this.model = new Model;
+
+console.log('init and things are made');
+
+};
--- main.js
+++ main.js
@@ -74,7 +74,7 @@ function Game_board() {
                 var ran_num = Math.floor(Math.random() * this.pieces_arr.length);
                 this.jewel_piece = {
                     x: x_cord,
-                    y: j
+                    y: j,
                     id: counter,
                     tile: this.pieces_arr[ran_num],//tile is how we'll determine what piece to place on the boar
                     info: $('<div>', {//class to add along with the x and y attributes for each dom element creating
@@ -86,7 +86,6 @@ function Game_board() {
                 };
 
                 $('.game_grid_container').append(this.jewel_piece.info.clone());//actual appending of the dom element to the html body
-                // $('game_grid_container').append(this.jewel_piece.info.clone());//actual appending of the dom element to the html body
                 multi_arr.push(this.jewel_piece);//each iteration of the inner loop pushes the value to the inner array
             }
             this.jewel_arr.push(multi_arr);
